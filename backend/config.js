08cc01497fdca7c2c2a530f3e20188bb72fe841f
Varun17-coder <varunandekar2002@gmail.com>
2025-03-05 12:52:34 +0530

    managed config.js file in backend

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-export const PORT=5555;
-
-//step 3
-export const mongoDBURL='mongodb+srv://root:book123@booklib.myfqp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookLib';
-// please create a free database. later delete it
